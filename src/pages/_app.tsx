db689e810c8da8c3e973348a83457c0a7d41ad21
Jonathan Dueck <jonathan@dueck.com.br>
2025-04-25 07:09:42 -0300

    Starting challange

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,108 @@
 import type { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
 
+import Image from "next/image";
+import { useState } from "react";
 import handBagIcon from "../assets/handbag.svg";
 import logoImg from "../assets/logo.svg";
+import xIcon from "../assets/x-icon.svg";
 import {
   CardIcon,
   Container,
   CountCardItems,
   Header,
+  ImageContainer,
+  ShoppingCartContainer,
+  ShoppingCartItem,
+  ShoppingCartItemDescription,
+  ShoppingCartItems,
+  XIconContainer,
 } from "../styles/pages/app";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isShoppingCartOpen, setIsShoppingCartOpen] = useState(false);
+
+  function handleOpenShoppingCart() {
+    setIsShoppingCartOpen(!isShoppingCartOpen);
+  }
+
   return (
     <Container>
       <Header>
         <img src={logoImg.src} alt="" />
-        <CardIcon>
+        <CardIcon onClick={handleOpenShoppingCart}>
           <img src={handBagIcon.src} alt="" />
-
           {true && <CountCardItems>1</CountCardItems>}
         </CardIcon>
       </Header>
+      <ShoppingCartContainer
+        css={
+          isShoppingCartOpen
+            ? { transform: "translateX(0%)" }
+            : { transform: "translateX(110%)" }
+        }
+      >
+        <XIconContainer>
+          <img onClick={handleOpenShoppingCart} src={xIcon.src} alt="" />
+        </XIconContainer>
+
+        <ShoppingCartItems>
+          <h2>Sacola de compras</h2>
+          <ShoppingCartItem>
+            <ImageContainer>
+              <Image
+                src={
+                  "https://files.stripe.com/links/MDB8YWNjdF8xUkJ3dEpRNDBuRE9IS0x3fGZsX3Rlc3RfcVZ1SVYwdlg4ejNhODFyNkxJWWZXOGpJ00NKhCvrVI"
+                }
+                alt=""
+                width={94}
+                height={94}
+              />
+            </ImageContainer>
+            <ShoppingCartItemDescription>
+              <span>Camiseta Beyond the limits</span>
+              <h1>R$ 79,90</h1>
+              <button>Remover</button>
+            </ShoppingCartItemDescription>
+          </ShoppingCartItem>
+          <ShoppingCartItem>
+            <ImageContainer>
+              <Image
+                src={
+                  "https://files.stripe.com/links/MDB8YWNjdF8xUkJ3dEpRNDBuRE9IS0x3fGZsX3Rlc3RfcVZ1SVYwdlg4ejNhODFyNkxJWWZXOGpJ00NKhCvrVI"
+                }
+                alt=""
+                width={94}
+                height={94}
+              />
+            </ImageContainer>
+            <ShoppingCartItemDescription>
+              <span>Camiseta Beyond the limits</span>
+              <h1>R$ 79,90</h1>
+              <button>Remover</button>
+            </ShoppingCartItemDescription>
+          </ShoppingCartItem>
+          <ShoppingCartItem>
+            <ImageContainer>
+              <Image
+                src={
+                  "https://files.stripe.com/links/MDB8YWNjdF8xUkJ3dEpRNDBuRE9IS0x3fGZsX3Rlc3RfcVZ1SVYwdlg4ejNhODFyNkxJWWZXOGpJ00NKhCvrVI"
+                }
+                alt=""
+                width={94}
+                height={94}
+              />
+            </ImageContainer>
+            <ShoppingCartItemDescription>
+              <span>Camiseta Beyond the limits</span>
+              <h1>R$ 79,90</h1>
+              <button>Remover</button>
+            </ShoppingCartItemDescription>
+          </ShoppingCartItem>
+        </ShoppingCartItems>
+      </ShoppingCartContainer>
       <Component {...pageProps} />
     </Container>
   );
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -55,3 +55,90 @@ export const CountCardItems = styled("div", {
   alignItems: "center",
   justifyContent: "center",
 });
+
+export const ShoppingCartContainer = styled("aside", {
+  position: "absolute",
+  width: "30rem",
+  height: "100%",
+  bottom: "0.25rem",
+  left: "100",
+  right: "0",
+  backgroundColor: "$gray800",
+  zIndex: 999,
+
+  transition: "all 0.2s ease-in-out",
+});
+
+export const XIconContainer = styled("div", {
+  position: "fixed",
+  top: "1.5rem",
+  right: "1.5rem",
+
+  cursor: "pointer",
+});
+
+export const ShoppingCartItems = styled("div", {
+  margin: "4.5rem 3rem",
+
+  display: "flex",
+  flexDirection: "column",
+  gap: "1.5rem",
+
+  h2: {
+    fontSize: "$lg",
+    fontWeight: "bold",
+    color: "$white",
+    lineHeight: "160%",
+    marginBottom: "2rem",
+  },
+});
+
+export const ShoppingCartItem = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  gap: "1.25rem",
+});
+
+export const ImageContainer = styled("div", {
+  width: "6.375rem",
+  height: "5.8rem",
+  background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
+  borderRadius: 8,
+  padding: "0.25rem",
+
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+
+  img: {
+    objectFit: "cover",
+  },
+});
+
+export const ShoppingCartItemDescription = styled("div", {
+  lineHeight: "160%",
+
+  span: {
+    color: "$gray100",
+    fontSize: "$md",
+  },
+
+  h1: {
+    color: "$white",
+    fontWeight: "bold",
+    fontSize: "$md",
+  },
+
+  button: {
+    background: "none",
+    border: "none",
+    fontWeight: "bold",
+    color: "$green500",
+    cursor: "pointer",
+    fontSize: "1rem",
+
+    "&:hover": {
+      color: "$green300",
+    },
+  },
+});
